e1a41a1935c1cfaed7a1f47a6826edc3cabaee4b
Anya Chan <anyagchan@gmail.com>
2024-03-18 13:58:18 -0400

    edits to search bar

--- my-installer/screens/Home.js
+++ my-installer/screens/Home.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ActivityIndicator } from 'react-native';
 import SearchBar from '../components/searchBar';
+import SearchResult from '../components/searchResult';
 
-function Home() {
+
+const Home = () => {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [fakeData, setFakeData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const apiResponse = await fetch(
+        "https://my-json-server.typicode.com/kevintomas1995/logRocket_searchBar/languages"
+      );
+      const data = await apiResponse.json();
+      setFakeData(data);
+    };
+    getData();
+  }, []);
+
   return (
     <View style = {styles.container}>
-      <SearchBar/>
-      <Text>Home</Text>
+      <SafeAreaView style={styles.root}>
+      <SearchBar
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
+      { !fakeData? (<ActivityIndicator size="large" />) : (
+
+          <SearchResult
+            searchPhrase={searchPhrase}
+            data={fakeData}
+            setClicked={setClicked}
+          />
+
+      )}
+    </SafeAreaView>
     </View>
   );
 }
@@ -17,6 +49,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  root: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    width: "100%",
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+    marginLeft: "10%",
   },
 })
 
